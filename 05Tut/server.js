2afd5316718def04ca57391754700c26a539c9d5
Sodiq Farhan <sodiqfarhan360@gmail.com>
2022-09-30 21:27:32 +0100

    Create a simple server that sends to the browser

--- 05Tut/server.js
+++ 05Tut/server.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
-const path = require('path');
-const http = require('http');
+const path = require('path')
+const http = require('http')
 
 const PORT = process.env.PORT || 3008;
 
